40432afa4f73dc8c8ef64f674c9491a69a9c565c
UP210409 <up210409@alumnos.upa.edu.mx>
2024-07-31 23:09:15 -0600

    Correccion ErrorHandler

--- backend/DBprueba/src/main/java/com/example/p02/config/ErrorHandler.java
+++ backend/DBprueba/src/main/java/com/example/p02/config/ErrorHandler.java
@@ -1,8 +1,8 @@
 package com.example.p02.config;
 
 import lombok.extern.slf4j.Slf4j;
-import com.example.p03.dto.ErrorDTO;
-import com.example.p03.exception.RuntimeException; //
+import com.example.p02.dto.ErrorDTO;
+import com.example.p02.exception.RuntimeException; //
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
