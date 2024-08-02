52c1f89c0449bab5383ea17dc8690aac974a6367
UP210409 <up210409@alumnos.upa.edu.mx>
2024-08-02 11:33:28 -0600

    Arreglo de Nav y Sidebar

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 // src/components/Nav/Nav.js
-
 import React from 'react';
 import './Nav.css';
+import { FaPlus, FaFilter } from 'react-icons/fa'; // Importa los íconos necesarios
 
 function Nav({ onAddTask, onFilterTasks }) {
   return (
@@ -10,8 +10,12 @@ function Nav({ onAddTask, onFilterTasks }) {
         <h1>JM Abogados</h1>
       </div>
       <div className="controls">
-        <button className="add-task" onClick={onAddTask}>+</button>
-        <button className="filter-task" onClick={onFilterTasks}>🔍</button>
+        <button className="add-task" onClick={onAddTask}>
+          <FaPlus />
+        </button>
+        <button className="filter-task" onClick={onFilterTasks}>
+          <FaFilter />
+        </button>
       </div>
     </div>
   );
