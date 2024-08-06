203d8c26ecbc53d9640fb22fb2e09b7185013dc3
UP210527 <up210527@alumnos.upa.edu.mx>
2024-08-06 16:29:22 -0600

    Otros cambios

--- backend/DBprueba/src/main/java/com/example/p02/controller/ProjectController.java
+++ backend/DBprueba/src/main/java/com/example/p02/controller/ProjectController.java
@@ -40,7 +40,7 @@ public class ProjectController {
     }
     
     @Operation(summary = "Mostrar todos los proyectos")
-    @GetMapping({"/all"})
+    @GetMapping
     public ResponseEntity<List<Project>> getProjects(){
         return ResponseEntity.ok(projectService.getProjects());
     }
