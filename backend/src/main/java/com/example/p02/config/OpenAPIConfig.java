655ee0a19f82d9c953b733dfb8327df56ce38a7b
UP210527 <up210527@alumnos.upa.edu.mx>
2024-08-07 00:10:36 -0600

    docker y api comentado

--- backend/src/main/java/com/example/p02/config/OpenAPIConfig.java
+++ backend/src/main/java/com/example/p02/config/OpenAPIConfig.java
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 
-@OpenAPIDefinition(
-    info = @Info(
-        title = "API Task",
-        description = "API para la administración de Task",
-        version = "v0.1.0"
-    )
-)
+// @OpenAPIDefinition(
+//     info = @Info(
+//         title = "API Task",
+//         description = "API para la administración de Task",
+//         version = "v0.1.0"
+//     )
+// )
 
 // http://localhost:8080/swagger-ui/index.html
 public class OpenAPIConfig {
